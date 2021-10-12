debaa929b93a0530047580ee987897d24fe3fce9
albinapotapova <albinkakam26@gmail.com>
2021-10-13 00:51:25 +0300

    add new test - test_guest_should_see_login_link

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,5 @@
 # импорт класса, описывающий главную страницу
-from .pages.main_page import MainPage
+from pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -8,4 +8,10 @@ def test_guest_can_go_to_login_page(browser):
     # открываем страницу
     page.open()
     # выполняем метод страницы — переходим на страницу логина
-    page.go_to_login_page()
+    page.go_to_login_page
+    
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
